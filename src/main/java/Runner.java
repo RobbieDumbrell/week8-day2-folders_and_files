70feec5ce67af3bfbfb003ec37b1b52793740ecf
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-08-14 19:21:39 +0100

    extensions complete with owner added

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,7 +1,9 @@
 import db.DBFolder;
 import db.DBHelper;
+import db.DBOwner;
 import models.File;
 import models.Folder;
+import models.Owner;
 
 import java.util.List;
 
@@ -9,10 +11,13 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        Folder folder1 = new Folder("Documents");
-        Folder folder2 = new Folder("Images");
-        Folder folder3 = new Folder("Programs");
-        Folder folder4 = new Folder("Untitled");
+        Owner owner1 = new Owner("Robbie", "robbieisthebest");
+        Owner owner2 = new Owner("Ellie", "ellielikesjelly");
+
+        Folder folder1 = new Folder("Documents", owner1);
+        Folder folder2 = new Folder("Images", owner2);
+        Folder folder3 = new Folder("Programs", owner1);
+        Folder folder4 = new Folder("Untitled", owner1);
 
         File file1 = new File("my_cv", "txt", 23, folder1);
         File file2 = new File("shopping_list", "txt", 15, folder1);
@@ -20,6 +25,9 @@ public class Runner {
         File file4 = new File("serious_pic", "png", 19, folder2);
         File file5 = new File("sad_pic", "png", 3, folder2);
         File file6 = new File("hello_world", "rb", 1, folder3);
+
+        DBHelper.save(owner1);
+        DBHelper.save(owner2);
 
         DBHelper.save(folder1);
         DBHelper.save(folder2);
@@ -44,7 +52,11 @@ public class Runner {
         DBHelper.delete(foundFile);
         DBHelper.delete(folder4);
 
+        Owner foundOwner = DBHelper.findById(Owner.class, 1);
+
         List<File> foundFiles = DBFolder.getFilesInFolder(foundFolder);
+
+        List<Folder> foundFolders = DBOwner.getFoldersOfOwner(foundOwner);
 
     }
 
